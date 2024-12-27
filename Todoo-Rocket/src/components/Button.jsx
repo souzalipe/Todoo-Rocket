b48c809d7de8468c9be79b9e312855ae920c4f4c
Felipe Nascimento de Souza <fn23886@gmail.com>
2024-12-27 17:31:45 -0300

    adiciona ícone de PlusCircle ao botão e inclui a dependência phosphor-react

--- Todoo-Rocket/src/components/Button.jsx
+++ Todoo-Rocket/src/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from "../styles/Button.module.css"
 
+import { PlusCircle } from 'phosphor-react';
+
 export function Button() {
     return (
       <button className={styles.container} type="submit">
-        Criar
+        Criar <PlusCircle width={16} height={16} />
       </button>
     )
   }
