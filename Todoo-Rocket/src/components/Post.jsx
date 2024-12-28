128971edffe171cfcd1265ebcc1aa67ee786da67
Felipe Nascimento de Souza <fn23886@gmail.com>
2024-12-28 16:13:21 -0300

    ajusta a estrutura do componente Post e adiciona estilos ao cabeçalho

--- Todoo-Rocket/src/components/Post.jsx
+++ Todoo-Rocket/src/components/Post.jsx
@@ -8,20 +8,32 @@ export function Post() {
   return (
     <article>
       <form>
-        <div style={{ display: "flex", justifyContent: "center", gap: "1rem", marginTop: "-2rem" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "1rem",
+            marginTop: "-2rem",
+          }}
+        >
           <Input />
           <Button />
         </div>
       </form>
-      <header>
-        <div>
-          <h3>Tarefas criadas</h3>
-        </div>
 
-        <div>
-          <h3>Concluídas</h3>
+      <header >
+        <div className={style.header}>
+          <div className={style.createdTaskContainer} >
+            <h3>Tarefas criadas</h3>
+          </div>
+
+          <div className={style.completedTaskContainer}>
+            <h3>Concluídas</h3>
+          </div>
         </div>
       </header>
+
+      <div></div>
     </article>
   );
 }
